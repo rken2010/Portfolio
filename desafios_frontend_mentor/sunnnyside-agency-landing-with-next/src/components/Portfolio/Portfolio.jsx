9d4c1377eba294fd13ae568de2770834cdff58cf
rken2010 <90514981+rken2010@users.noreply.github.com>
2023-04-04 13:53:29 -0300

    add format in images

--- desafios_frontend_mentor/sunnnyside-agency-landing-with-next/src/components/Portfolio/Portfolio.jsx
+++ desafios_frontend_mentor/sunnnyside-agency-landing-with-next/src/components/Portfolio/Portfolio.jsx
@@ -9,16 +9,16 @@ export default function Portfolio( ) {
 
   <section className={styles.portfolio__container}>     
     <div className={styles.portfolio__container__mobile}>
-      <Image src="/img/mobile/image-gallery-milkbottles.jpg" alt="milkbottles"/>
-      <Image src="/img/mobile/image-gallery-orange.jpg" alt="orange"/>
-      <Image src="/img/mobile/image-gallery-cone.jpg" alt="cone"/>
-      <Image src="/img/mobile/image-gallery-sugar-cubes.jpg" alt="sugar cubes"/>
+      <Image src="/img/mobile/image-gallery-milkbottles.jpg" alt="milkbottles" width="100%"/>
+      <Image src="/img/mobile/image-gallery-orange.jpg" alt="orange" width="100%"/>
+      <Image src="/img/mobile/image-gallery-cone.jpg" alt="cone" width="100%"/>
+      <Image src="/img/mobile/image-gallery-sugar-cubes.jpg" alt="sugar cubes" width="100%"/>
     </div>
     <div className={styles.portfolio__container__desktop}>
-     <Image src="/img/desktop/image-gallery-milkbottles.jpg" alt="milkbottles"/>
-     <Image src="/img/desktop/image-gallery-orange.jpg" alt="orange"/>
-     <Image src="/img/desktop/image-gallery-cone.jpg" alt="cone"/>
-     <Image src="/img/desktop/image-gallery-sugarcubes.jpg" alt="sugar cubes"/>
+     <Image src="/img/desktop/image-gallery-milkbottles.jpg" alt="milkbottles" width="100%"/>
+     <Image src="/img/desktop/image-gallery-orange.jpg" alt="orange" width="100%"/>
+     <Image src="/img/desktop/image-gallery-cone.jpg" alt="cone" width="100%"/>
+     <Image src="/img/desktop/image-gallery-sugarcubes.jpg" alt="sugar cubes" width="100%"/>
      </div>
   </section>
 
