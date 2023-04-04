035077957e054e8b8da657b8f65cfdff8800f838
rken2010 <90514981+rken2010@users.noreply.github.com>
2023-04-04 12:36:57 -0300

    add styles with modules in Portfolio section

--- desafios_frontend_mentor/sunnnyside-agency-landing-with-next/src/components/Portfolio/Portfolio.jsx
+++ desafios_frontend_mentor/sunnnyside-agency-landing-with-next/src/components/Portfolio/Portfolio.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
-import { Image, Box, Stack } from '@chakra-ui/react'
+import Image from 'next/image'
+import styles from "Portfolio.module.css"
+
+
 
 
 export default function Portfolio( ) {
   
   return (
-    <Stack
-      direction={{base:"column", md:"row"}}
-      as="section"
-    >
-    <Box
-      display={{basic:"flex", md:"none"}}
-      flexWrap ="wrap"     
-    >
+
+  <section className={styles.portfolio__container}>     
+    <div className={styles.portfolio__container__mobile}>
       <Image src="/img/mobile/image-gallery-milkbottles.jpg" alt="milkbottles"/>
       <Image src="/img/mobile/image-gallery-orange.jpg" alt="orange"/>
       <Image src="/img/mobile/image-gallery-cone.jpg" alt="cone"/>
       <Image src="/img/mobile/image-gallery-sugar-cubes.jpg" alt="sugar cubes"/>
-    </Box>
-     <Box
-     flexWrap ="wrap"
-     display={{basic:"none", md:"flex"}}
- >
+    </div>
+    <div>
      <Image src="/img/desktop/image-gallery-milkbottles.jpg" alt="milkbottles"/>
      <Image src="/img/desktop/image-gallery-orange.jpg" alt="orange"/>
      <Image src="/img/desktop/image-gallery-cone.jpg" alt="cone"/>
      <Image src="/img/desktop/image-gallery-sugarcubes.jpg" alt="sugar cubes"/>
-    </Box>
-  </Stack>
+     </div>
+  </section>
 
   )
 }
