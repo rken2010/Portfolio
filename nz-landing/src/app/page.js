9b4d61ec6009309380a877c1e64a28968486fb65
rken2010 <rkenshin.2010@gmail.com>
2023-07-13 19:30:36 -0300

    style: :construction: add styles

--- nz-landing/src/app/page.js
+++ nz-landing/src/app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import Head from 'next/head'
+
 import ServiceContainer from '@/components/ServiceContainer/ServiceContainer'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -17,6 +19,12 @@ export default function Home() {
   return (
     <CacheProvider>
       <ChakraProvider  theme={theme}>
+        <Head>
+        <title>NZ</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Martel&family=Parisienne&family=Trykker&display=swap" rel="stylesheet"/>
+        </Head>
         <Header />
         <Initial />
         <ServiceContainer servicesList={template.servicios}/> 
